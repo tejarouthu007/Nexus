28b533d1312255310120f0ea0fe1c2d98be4631b
tejarouthu007 <tejarouthu2004@gmail.com>
2025-05-06 00:10:27 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://nexus-editor-live.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true,
     }
 });
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://nexus-editor-live.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true,
 }));
